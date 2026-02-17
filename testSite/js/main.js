33c44d95c86c466c6db34c7a22dfd4da57ec314a
VViller <vviller959@gmail.com>
2026-02-17 16:58:13 +0500

    170226_1

--- testSite/js/main.js
+++ testSite/js/main.js
@@ -5,20 +5,20 @@ document
   .querySelector(".design-flex__btn")
   .addEventListener("click", openModal);
 
-function closеModal() {
+function clouseModal() {
   document.querySelector(".first-modal").style.display = "none";
   messageFild.value = "";
 }
-document.querySelector(".clouse__btn").addEventListener("click", closеModal);
+document.querySelector(".clouse__btn").addEventListener("click", clouseModal);
 
 // кнопка и отправка текста из поля на почту
 let sendBtn = document.querySelector(".send__btn");
-let messageFild = document.querySelector(".first-modal-mesage");
+let messageFild = document.querySelector(".first-modal-message");
 
 sendBtn.addEventListener("click", function () {
   let message = messageFild.value;
 
-  let email = "login841@gmail.com";
+let email = "vviller959@gmail.com";
   let subject = "Вопрос по разработке";
   let mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
   window.location.href = mailtoLink;
